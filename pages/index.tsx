1b82c14d8a34179685a711ebaeb98aaacb9a2cb1
Cerebrovinny <vinny.cardoso96@gmail.com>
2022-11-05 10:15:10 -0300

    added skills component

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import WorkExperience from "../components/WorkExperience";
+import Skills from "../components/Skills";
 
 const Home: NextPage = () => {
   return (
@@ -31,6 +32,11 @@ const Home: NextPage = () => {
         {/*Experience*/}
         <section id="experience" className="snap-center">
             <WorkExperience />
+        </section>
+
+        {/*Skills*/}
+        <section id="skills" className="snap-start">
+            <Skills />
         </section>
         <h1 className="p-10 text-red-500">awesome s</h1>
 </div>
