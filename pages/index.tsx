d1a1ec00dc257a8a91f69887fd268f7118289706
Cerebrovinny <vinny.cardoso96@gmail.com>
2022-11-19 19:19:27 -0300

    fix scroll bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import About from "../components/About";
@@ -11,43 +11,45 @@ import Projects from "../components/Projects";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory
-    overflow-scroll z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
+    <div
+      className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory
+    overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80"
+    >
       <Head>
-          <title>Vinicius Portfolio</title>
+        <title>Vinicius Portfolio</title>
       </Head>
 
-        {/*Header*/}
-        <Header />
+      {/*Header*/}
+      <Header />
 
-        {/*Hero*/}
-        <section id="hero" className="snap-start">
-            <Hero />
-        </section>
+      {/*Hero*/}
+      <section id="hero" className="snap-start">
+        <Hero />
+      </section>
 
-        {/*About*/}
-        <section id="about" className="snap-center">
-            <About />
-        </section>
+      {/*About*/}
+      <section id="about" className="snap-center">
+        <About />
+      </section>
 
-        {/*Experience*/}
-        <section id="experience" className="snap-center">
-            <WorkExperience />
-        </section>
+      {/*Experience*/}
+      <section id="experience" className="snap-center">
+        <WorkExperience />
+      </section>
 
-        {/*Skills*/}
-        <section id="skills" className="snap-start">
-            <Skills />
-        </section>
+      {/*Skills*/}
+      <section id="skills" className="snap-start">
+        <Skills />
+      </section>
 
-        {/*projects*/}
-        <section id="projects" className="snap-start">
-            <Projects />
-        </section>
+      {/*projects*/}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
-        {/* Todo contact me */}
-</div>
-  )
-}
+      {/* Todo contact me */}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
